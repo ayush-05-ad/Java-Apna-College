64d51818ced292e91a6fda5c1d1eb99f2740713a
Ayush Deep <76211201+ayush-05-ad@users.noreply.github.com>
2026-08-18 01:27:10 +0000

    Add first in LL.

---
+++ LinkedList-Part1/AddFirst_LL.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+
+public class AddFirst_LL {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public static Node head;
+        public static Node tail;
+
+        public void addFirst(int data) {
+
+            // step 1 -> Create new Node
+            Node newNode = new Node(data);
+
+            // this step only for null case
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            }
+
+            // step2 -> newNode next = head
+            newNode.next = head; // Link
+
+            // step3 -> head = newNode
+            head = newNode;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(2);
+        ll.addFirst(1);
+    }
+}
