1ed659140f1df4798ba6cc698d40106a2790750d
Ayush Deep <76211201+ayush-05-ad@users.noreply.github.com>
2026-08-19 17:09:10 +0000

    Creation of ArrayList.

---
+++ ArrayList/Introduction.java
@@ -0,0 +1,13 @@
+import.java.util.ArrayList;
+
+public class Introduction {
+public static void main (String args []){
+    //String or Integer
+    ArrayList<Integer> list = new ArrayList <> ();
+
+    //ClassName ObjectName = New ClassName ();
+    //ArrayList is part of java Collection
+
+    ArrayList <String> list2 = new ArrayList<> ();
+}
+}
